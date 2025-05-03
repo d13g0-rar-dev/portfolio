7b3805413381bfbf6adce988b431d0b1771f46b6
d13g0-rar-dev <diego2210063@correo.uis.edu.co>
2025-05-02 19:45:32 -0500

    Remove merge conflict markers from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,4 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> origin/master
